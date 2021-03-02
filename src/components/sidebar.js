ad5bcd975daa5ae83e207e55bb856a55b4a6b00a
Abhi <abhishekmrm1998@gmail.com>
2021-03-02 15:13:31 +0530

    some changes"

---
+++ src/components/sidebar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Side = (
+  <div>
+    <ul>
+      <li>
+        <a href=""></a>
+      </li>
+    </ul>
+  </div>
+);
+const Sidebar = () => {
+  return Side;
+};
+
+export default Sidebar;
